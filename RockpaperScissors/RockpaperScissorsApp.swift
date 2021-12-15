255b170d69bd66f49eed878e6c1569a0040bca89
armanjakhayev <67386397+armanjakhayev@users.noreply.github.com>
2021-12-15 13:40:10 -0800

    Initial Commit

---
+++ RockpaperScissors/RockpaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockpaperScissorsApp.swift
+//  RockpaperScissors
+//
+//  Created by Arman on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RockpaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
